4be328d3cda326aa4e981c197ad204d2e4de6b19
Tracy <tracy100001@outlook.com>
2025-08-04 10:24:22 +0100

    fix: configure nginx

--- setup_nginx.sh
+++ setup_nginx.sh
@@ -2,10 +2,10 @@
 
 # === Config ===
 SERVER_USER="ubuntu"
-SERVER_HOST="ec2-18-116-21-198.us-east-2.compute.amazonaws.com"
+SERVER_HOST="ec2-18-223-109-109.us-east-2.compute.amazonaws.com"
 SSH_KEY="$HOME/Downloads/sols-keypair.pem"
-DOMAIN="solsticecinematics.com"
-TARGET_PORT="3000"  # Docker app port
+DOMAIN="studio.solsticecinematics.com"
+TARGET_PORT="3001"  # Docker app port
 
 # === SSH Command ===
 ssh -i "$SSH_KEY" $SERVER_USER@$SERVER_HOST << EOF
@@ -44,7 +44,7 @@ server {
 }
 EOL
 
-  echo "Enabling site and reloading Nginx..."
+  echo "Enabling site and restarting Nginx..."
   sudo ln -sf /etc/nginx/sites-available/$DOMAIN /etc/nginx/sites-enabled/
   sudo nginx -t && sudo systemctl restart nginx
 
